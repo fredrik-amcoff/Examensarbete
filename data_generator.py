94d52916074382bdc161e0bac7e95b4c3e1015b0
Fredrik Amcoff <fredrik.amcoff@gmail.com>
2025-04-14 10:08:38 +0200

    Minor updates

--- data_generator.py
+++ data_generator.py
@@ -20,7 +20,9 @@ def generate_text(title, length, key):
     client = openai.OpenAI(api_key=key)
     response = client.chat.completions.create(
         model="gpt-4o-mini",
-        messages=[{"role": "user", "content": f"Write a text about {title} in around {length} words."}],
+        messages=[{"role": "user", "content": f'Write a text about the topic of "{title}" in around {length} words.'}],
+        temperature=1,
+        seed=587078
     )
     text_response = response.choices[0].message.content
     return text_response
@@ -39,7 +41,7 @@ def get_wikipedia_text(title, user_agent, num_paragraphs=3):
     return output_text, len(words)
 
 
-def store_data(titles, key, user_agent, num_paragraphs=3):
+def store_data_from_list(titles, key, user_agent, num_paragraphs=3):
     data = []
     times = []
     for title in titles:
@@ -205,109 +207,6 @@ def sample_articles(num_articles, user_agent, num_paragraphs=3):
 # for user: contact information of the format "<Application Name>/<Version> (<Description>, <Contact Information>)",
 # example: "ReasearchProject/1.0 (University of Uppsala, contact: email.example@student.uu.se)"
 user = ""
-token = ""  # OpenAI API key
-
-wikipedia_topics = [
-    "Quantum mechanics",
-    "Artificial intelligence",
-    "Theory of relativity",
-    "CRISPR",
-    "Black hole",
-    "Nanotechnology",
-    "Renewable energy",
-    "Machine learning",
-    "History of the Internet",
-    "SpaceX",
-    "Fall of the Western Roman Empire",
-    "Cold War",
-    "French Revolution",
-    "Nelson Mandela",
-    "Civil rights movement",
-    "History of democracy",
-    "Industrial Revolution",
-    "Cuban Missile Crisis",
-    "World War I",
-    "Mesopotamia",
-    "Great Wall of China",
-    "Amazon rainforest",
-    "Sahara Desert",
-    "Mariana Trench",
-    "Mount Everest",
-    "Atlantis",
-    "Aurora (astronomy)",
-    "Great Barrier Reef",
-    "Wonders of the World",
-    "Climate change",
-    "William Shakespeare",
-    "Mona Lisa",
-    "Science fiction",
-    "Art and politics",
-    "Greek mythology",
-    "Vincent van Gogh",
-    "Graphic novel",
-    "Anime",
-    "Surrealism",
-    "Digital art",
-    "Existentialism",
-    "Stoicism",
-    "Buddhism",
-    "Ethics of artificial intelligence",
-    "Religion in ancient Rome",
-    "Free will",
-    "Confucianism",
-    "Vatican City",
-    "Meaning of life",
-    "Atheism",
-    "Discovery of penicillin",
-    "Vaccine",
-    "Human microbiome",
-    "Sleep",
-    "Stress (biology)",
-    "Nutrition and mental health",
-    "Anesthesia",
-    "Physical exercise",
-    "Gut microbiota",
-    "Black Death",
-    "Olympic Games",
-    "Chess",
-    "Formula One",
-    "Sports psychology",
-    "Esports",
-    "FIFA World Cup",
-    "Martial arts",
-    "Tour de France",
-    "Sports rivalry",
-    "History of basketball",
-    "Rock and roll",
-    "Hip hop music",
-    "The Beatles",
-    "Jazz",
-    "Animation",
-    "Golden Age of Hollywood",
-    "Music streaming service",
-    "K-pop",
-    "Video game console",
-    "Horror film",
-    "Dot-com bubble",
-    "Cryptocurrency",
-    "Stock market",
-    "Central bank",
-    "Gig economy",
-    "Economic impact of the COVID-19 pandemic",
-    "Globalization",
-    "Universal basic income",
-    "Silicon Valley",
-    "Space economy",
-    "History of timekeeping devices",
-    "Conspiracy theory",
-    "Memory",
-    "April Fools' Day",
-    "Déjà vu",
-    "Dark matter",
-    "Playing card",
-    "List of languages by number of speakers",
-    "Humanoid robot",
-    "Luck"
-]
+token = ""
 
 store_data_from_random(5000, token, user)
