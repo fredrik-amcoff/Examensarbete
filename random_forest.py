f8975f7f4916d9f5a749520123b8d3690db86061
Oliver <oliver.bjork2001@gmail.com>
2025-04-29 12:46:26 +0200

    added updated isntrinsicdim to text statistics files

---
+++ random_forest.py
@@ -0,0 +1,6 @@
+import numpy
+import pandas
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+
